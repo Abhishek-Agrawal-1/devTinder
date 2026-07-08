ae10cd3b5fcd74336738ebdb44682abbcd9cc3e7
Abhishek Agrawal <25abhishekagrawal@gmail.com>
2026-07-08 10:30:15 +0530

    exploring GET, POST, DEL HTTP requests in express

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the test!");
+
+// this will only handle the get request for /user route
+app.get("/user", (req, res) => {
+    res.send({ firstName : "ABHISHEK", lastName : "AGRAWAL"});
+})
+
+app.post("/user", (req, res) => {
+    res.send("user created succesfully!");
 });
 
-app.use("/hello", (req, res) => {
-    res.send("Hello from the hello route!");
-});
+app.delete("/user", (req, res) => {
+    res.send("user deleted succcesfully!");
+})
 
-app.use("/admin", (req, res) => {
-    res.send("Hello from the admin route!");
-});
 
-app.use("/", (req, res) => {
-    res.send("Hello from the Server!");
-});// request handler
+//this will match all the HTTP method API calls to /user route
+app.use("/user", (req, res) => {
+    res.send("user updated succesfully!");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
