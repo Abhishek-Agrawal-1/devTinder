f2c277c50707c8a7d1db744b34f03c32e074769b
Abhishek Agrawal <25abhishekagrawal@gmail.com>
2026-07-09 01:14:01 +0530

    wildCard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const { adminAuth , userAuth} = require('./middleware/auth');
-
-app.use("/admin", adminAuth);
-app.use("/user", userAuth);
-
-app.get("/user/data", userAuth, (req, res) => {
-    res.send("user data sent");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All data is sent");
+app.get("/", (err, req, res, next) => {
+    if(err) {
+        res.status(500).send("Internal server error");
+    }
 })
 
-app.get("/admin/deleteUser", (req, res) => {
-    res.send("Deleted User");
+app.get("/getUserData", (req, res) => {
+    try {
+        throw new Error("User data not found");
+        res.send("User data retrieved successfully");
+    }catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.get("/", (err, req, res, next) => {
+    if(err) {
+        res.status(500).send("Internal server error");
+    }
 })
 
 app.listen(PORT, () => {
