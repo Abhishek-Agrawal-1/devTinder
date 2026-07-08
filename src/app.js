5de3c3aa029e2719ba8d27bc6049a662cbc88203
Abhishek Agrawal <25abhishekagrawal@gmail.com>
2026-07-08 16:59:16 +0530

    multiple-route handlers

--- src/app.js
+++ src/app.js
@@ -2,37 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send({ firstName : "ABHISHEK", lastName : "AGRAWAL"});
-})
-
-app.get("/user", (req, res) => {
-    console.log(req.query);
-    res.send({ firstName : "ABHISHEK", lastName : "AGRAWAL"});
-})
-
-
-
-// this will only handle the get request for /user route
-app.get("/user", (req, res) => {
-    res.send({ firstName : "ABHISHEK", lastName : "AGRAWAL"});
-})
-
-app.post("/user", (req, res) => {
-    res.send("user created succesfully!");
+app.use("/user", (req, res, next) => {
+    console.log("request handler 1");
+    next();
+}, (req, res, next) => {
+    console.log("request handler 2");
+    next();
+}, (req, res, next) => {
+    console.log("request handler 3");
+    next();
+}, (req, res, next) => {
+    console.log("request handler 4");
+    next();
+}, (req, res, next) => {
+    console.log("request handler 5");
+    res.send("response 5");
 });
-
-app.delete("/user", (req, res) => {
-    res.send("user deleted succcesfully!");
-})
-
-
-//this will match all the HTTP method API calls to /user route
-app.use("/user", (req, res) => {
-    res.send("user updated succesfully!");
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
