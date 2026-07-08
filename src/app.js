1ccd8431f756fecae1e2e2c5da43f78479d9e1ca
Abhishek Agrawal <25abhishekagrawal@gmail.com>
2026-07-08 14:12:34 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ const app = express();
 const PORT = 3000;
 
 
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({ firstName : "ABHISHEK", lastName : "AGRAWAL"});
+})
+
+app.get("/user", (req, res) => {
+    console.log(req.query);
+    res.send({ firstName : "ABHISHEK", lastName : "AGRAWAL"});
+})
+
+
+
 // this will only handle the get request for /user route
 app.get("/user", (req, res) => {
     res.send({ firstName : "ABHISHEK", lastName : "AGRAWAL"});
