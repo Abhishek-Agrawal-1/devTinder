3465073716e692e2e11eeb314d3174c343ca448f
Abhishek Agrawal <25abhishekagrawal@gmail.com>
2026-07-18 04:40:17 +0530

    created userSchema method to getJWT() and comparePassword(passwordInputByUser)

--- src/app.js
+++ src/app.js
@@ -63,16 +63,12 @@ app.post("/login", async (req, res) => {
       throw new Error("Incorrect Credentials");
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
 
-      //creating a JWT token
-      const token = await jwt.sign({ _id: user._id }, "Abhishek@123$3", { 
-        expiresIn : "1d", 
-      });
+      const token = await user.getJWT();
 
-      //Add the token to cookie and send response back to the user
       res.cookie("token", token , {
         expires : new Date(Date.now() + 8 * 3600000),
       });
--- src/models/user.js
+++ src/models/user.js
@@ -1,70 +1,94 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    firstName : {
-        type : String,
-        required : true,
-        minlength : 2,
-        maxLength : 50,
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxLength: 50,
     },
-    lastName : {
-        type : String,
+    lastName: {
+        type: String,
         // minlength : 2,
         // maxLength : 50,
     },
-    emailId : {
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true,
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
         validate(value) {
-            if(!validator.isEmail(value)){
+            if (!validator.isEmail(value)) {
                 throw new Error("Invalid Email Address " + value);
             }
         }
     },
-    password : {
-        type : String,
-        required : true,
+    password: {
+        type: String,
+        required: true,
         validate(value) {
-            if(!validator.isStrongPassword(value)){
+            if (!validator.isStrongPassword(value)) {
                 throw new Error("Enter a Strong Password : " + value);
             }
         }
     },
-    gender : {
-        type : String,
+    gender: {
+        type: String,
         validate(value) {
-            if(!["Male" , "Female" , "other"].includes(value)){
+            if (!["Male", "Female", "other"].includes(value)) {
                 throw new Error("Gender data is not valid");
             }
         },
     },
-    age : {
-        type : Number,
-        min : 15,
-        max : 100,
+    age: {
+        type: Number,
+        min: 15,
+        max: 100,
     },
-    photoUrl : {
-        type : String,
-        default : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpy8fSabmG_TcZLtTTiedr81bYgG0DdIkT2lpRKLgl5Q&s=10",
+    photoUrl: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpy8fSabmG_TcZLtTTiedr81bYgG0DdIkT2lpRKLgl5Q&s=10",
         validate(value) {
-            if(!validator.isURL(value)){
+            if (!validator.isURL(value)) {
                 throw new Error("Invalid photo URL : " + value);
             }
         }
     },
-    about : {
-        type : String,
-        default : "this is the default about of the user",
-        maxLength : 150,
+    about: {
+        type: String,
+        default: "this is the default about of the user",
+        maxLength: 150,
     },
-    skills : {
-        type : [String],
+    skills: {
+        type: [String],
     },
-}, { timestamps : true});
+}, { timestamps: true });
+
+
+
+userSchema.methods.getJWT() = async function () {
+
+    const user = this;
+
+    const token = await jwt.sign({ _id: user._id }, "Abhishek@123$3", {
+        expiresIn: "1d",
+    });
+
+    return token;
+};
+
+
+userSchema.methods.validatePassword() = async function (passwordInputByUser) {
+    const user = this;
+    const passwordHash = user.password;
+
+    const isValidPassword = await bcrypt.compare(passwordInputByUser, passwordHash);
+
+    return isValidPassword;
+}
 
 
 module.exports = mongoose.model('User', userSchema);
